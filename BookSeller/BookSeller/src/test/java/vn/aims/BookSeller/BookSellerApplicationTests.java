c071e555573afc5b33b758bd6e9e83e95ebc2f05
nghiaduong <duongnghia2904204@gmail.com>
2025-07-07 12:53:34 +0700

    test

--- BookSeller/BookSeller/src/test/java/vn/aims/BookSeller/BookSellerApplicationTests.java
+++ BookSeller/BookSeller/src/test/java/vn/aims/BookSeller/BookSellerApplicationTests.java
@@ -1,10 +1,21 @@
 package vn.aims.BookSeller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import vn.aims.BookSeller.DTO.request.UserCreationDTO;
+import vn.aims.BookSeller.Service.UserService;
 
+@Slf4j
+@AutoConfigureMockMvc
 @SpringBootTest
 class BookSellerApplicationTests {
+	@MockBean
+	private UserService userService;
+	private UserCreationDTO userCreationDTO;
+
 
 	@Test
 	void contextLoads() {
